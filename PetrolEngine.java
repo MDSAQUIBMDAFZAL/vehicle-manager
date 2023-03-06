a34667e0f3c1d6382504eb61aa6e3d67de1c9d5c
ansar <ansarisaquib9225@gmail.com>
2023-03-06 20:16:38 +0530

    again changed

---
+++ PetrolEngine.java
@@ -0,0 +1,6 @@
+public class PetrolEngine {
+    public static void start() {
+        System.out.println("started");
+    }
+    
+}
--- Vehicle.java
+++ Vehicle.java
@@ -1,7 +1,7 @@
 public class Vehicle {
-    private PerolEngine pE;
+    
 	public static void main(String[] args){
-        System.out.println("hello world");
-        pE.start();
+        private PetrolEngine pE=new PetrolEngine();
+                pE.start();
     }
 }
