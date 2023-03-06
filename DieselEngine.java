c990dd629ac7ae5efd322bd85c3ebb28995abc5a
ansar <ansarisaquib9225@gmail.com>
2023-03-06 20:36:07 +0530

    only two files changed

--- DieselEngine.java
+++ DieselEngine.java
@@ -1,5 +1,9 @@
 public class DieselEngine {
     public static void start() {
-        System.out.println("started");
+        System.out.println("Diesel Enginestarted");
+    }
+    
+    public static void stop() {
+        System.out.println("Diesel Enginestopped");
     }
 }
--- PetrolEngine.java
+++ PetrolEngine.java
@@ -1,6 +1,10 @@
 public class PetrolEngine {
     public static void start() {
-        System.out.println("started");
+        System.out.println("Petrol Engine started");
+    }
+    
+    public static void stop() {
+        System.out.println("Petrol Engine stopped");
     }
     
 }
